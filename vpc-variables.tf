380745b33d4283ed9b0da786621a701ec2477b21
daringanitch <dganitch@gmail.com>
2020-07-10 14:46:40 -0700

    Update vpc-variables.tf

--- vpc-variables.tf
+++ vpc-variables.tf
@@ -3,18 +3,18 @@ variable "aws_secret_key" {}
 
 variable "aws_region" {
     description = "EC2 Region for the VPC"
-    default = "us-west-2"
+    default = "us-gov-east-1"
 }
 variable "availability_zone1" {
     description = "Avaialbility Zones"
-    default = "us-west-2a"
+    default = "us-gov-east-1a"
 }
 
 variable "availability_zone2" {
     description = "Avaialbility Zones"
-    default = "us-west-2b"
+    default = "us-gov-east-1b"
 }
-variable "main_vpc_cidr" {
+variable "main2_vpc_cidr" {
     description = "CIDR of the VPC"
-    default = "10.0.0.0/16"
+    default = "10.202.0.0/16"
 }
